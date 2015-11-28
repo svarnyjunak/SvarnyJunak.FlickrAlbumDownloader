cf55268c76192815125e838322dda19b48aab669
svarnyjunak <svarnyjunak@gmail.com>
2015-11-28 23:29:27 +0100

    initial commit

---
+++ FlickrAlbumDownloader/FlickrDownloader.cs
@@ -0,0 +1,116 @@
+﻿using System.Net;
+using FlickrNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace FlickrAlbumDownloader
+{
+    public class FlickrDownloader
+    {
+        private readonly Flickr _flickr;
+
+        public FlickrDownloader(string apiKey, string sharedSecret)
+        {
+            _flickr = new Flickr(apiKey, sharedSecret);
+        }
+
+        public OAuthRequestToken GetAuthRequestToken()
+        {
+            var requestToken = _flickr.OAuthGetRequestToken("oob");
+            
+            return new OAuthRequestToken
+            {
+                Token = requestToken.Token,
+                TokenSecret = requestToken.TokenSecret
+            };
+        }
+
+        public string GetAuthUrl(OAuthRequestToken token)
+        {
+            return _flickr.OAuthCalculateAuthorizationUrl(token.Token, AuthLevel.Read);
+        }
+
+        public void SetAuthCode(OAuthRequestToken token, string code)
+        {
+            var accessToken = _flickr.OAuthGetAccessToken(token.Token, token.TokenSecret, code);
+            _flickr.OAuthAccessToken = accessToken.Token;
+            _flickr.OAuthAccessTokenSecret = accessToken.TokenSecret;
+        }
+
+        public IEnumerable<PhotoSet> GetPhotoSets(string userId)
+        {
+            var photosets = _flickr.PhotosetsGetList(userId);
+            var order = photosets.Count;
+            var result = new List<PhotoSet>();
+
+            foreach (var photoset in photosets)
+            {
+                result.Add(new PhotoSet
+                {
+                    Id = photoset.PhotosetId,
+                    Name = photoset.Title,
+                    Order = order,
+                });
+
+                order--;
+            }
+
+            return result.OrderBy(p => p.Order);
+        }
+
+        public void DownloadPhotoSet(string photoSetId, string path)
+        {
+            var client = new WebClient();
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            var photos = _flickr.PhotosetsGetPhotos(photoSetId, PhotoSearchExtras.OriginalUrl | PhotoSearchExtras.DateTaken);
+            var currentPhoto = 0;
+
+            foreach (var photo in photos)
+            {
+                currentPhoto++;
+
+                var originalPhotoUrl = photo.OriginalUrl;
+                var fileName = photo.DateTaken.ToString("yyy-MM-dd-HH-mm-ss-") + photo.PhotoId + ".jpg";
+                var finalPath = Path.Combine(path, fileName);
+                client.DownloadFile(originalPhotoUrl, finalPath);
+                File.SetCreationTime(finalPath, photo.DateTaken);
+                File.SetLastWriteTime(finalPath, photo.DateTaken);
+                OnPhotoSetDownloadProgressChanged(new PhotoSetDownloadProgress { CurrentPhotoNumber = currentPhoto, PhotosCount = photos.Count });
+            }
+        }
+
+        protected void OnPhotoSetDownloadProgressChanged(PhotoSetDownloadProgress progress)
+        {
+            if (PhotoSetDownloadProgressChanged != null)
+            {
+                PhotoSetDownloadProgressChanged(this, progress);
+            }
+        }
+
+        public event EventHandler<PhotoSetDownloadProgress> PhotoSetDownloadProgressChanged;
+    }
+
+    public struct OAuthRequestToken
+    {
+        public string Token { get; set; }
+        public string TokenSecret { get; set; }
+    }
+
+    public struct PhotoSet
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+
+    public struct PhotoSetDownloadProgress
+    {
+        public int PhotosCount { get; set; }
+        public int CurrentPhotoNumber { get; set; }
+    }
+}
---
+++ FlickrAlbumDownloader/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FlickrAlbumDownloader.Properties;
+
+namespace FlickrAlbumDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var downloader = new FlickrDownloader(Settings.Default.FlickrApiKey, Settings.Default.FlickrSharedSecret);
+
+                if (Settings.Default.UseAuthetication)
+                {
+                    var token = downloader.GetAuthRequestToken();
+                    var url = downloader.GetAuthUrl(token);
+
+                    System.Diagnostics.Process.Start(url);
+
+                    var code = GetData("Authorize this application and write the code from the flickr page:");
+                    Console.WriteLine();
+                    downloader.SetAuthCode(token, code);
+                }
+
+                var photosets = downloader.GetPhotoSets(Properties.Settings.Default.UserId).ToArray();
+
+                Console.WriteLine("Which photoset do you want to download?");
+
+                foreach (var photoset in photosets)
+                {
+                    Console.WriteLine("{0} - {1}", photoset.Order, photoset.Name);
+                }
+
+                Console.WriteLine();
+                var order = GetData("Write photoset id, press Enter and wait a few seconds:");
+                Console.WriteLine();
+                Console.WriteLine("I'm warming up.");
+                var selectedPhotoSet = (from p in photosets where p.Order.ToString() == order select p).Single();
+
+                var path = Path.Combine(Properties.Settings.Default.DownloaderPath, selectedPhotoSet.Id);
+
+                downloader.PhotoSetDownloadProgressChanged += downloader_PhotoSetDownloadProgressChanged;
+                downloader.DownloadPhotoSet(selectedPhotoSet.Id, path);
+                Console.WriteLine("And we're done");
+                Console.WriteLine("All photos has been downloaded to {0}.", path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Upps. Something wrong's happend.");
+                Console.WriteLine("Here are some details:");
+                Console.Write(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        static void downloader_PhotoSetDownloadProgressChanged(object sender, PhotoSetDownloadProgress e)
+        {
+            Console.WriteLine("{0} of {1} photos has been downloaded.", e.CurrentPhotoNumber, e.PhotosCount);
+        }
+
+        private static string GetData(string caption)
+        {
+            Console.WriteLine(caption);
+            return Console.ReadLine();
+        }
+    }
+}
